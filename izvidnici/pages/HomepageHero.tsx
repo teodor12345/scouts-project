5b0097398eba89aaaa15dbd7538ba301c9639760
Teodor Madzovski <ted.madzovski@gmail.com>
2023-03-24 17:02:21 +0100

    1 commit
    adding react icons

---
+++ izvidnici/pages/HomepageHero.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+function HomepageHero (){
+
+    return <h1>Извиднички Клуб Наум Наумовски-Борче </h1>
+
+}
+
+export default HomepageHero
--- izvidnici/pages/_app.tsx
+++ izvidnici/pages/_app.tsx
@@ -2,5 +2,7 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return<>
+ <p className=' font-bold'>This id my app lets see what we can do with it and learn something new I hope really hope because it is not easy!!</p>
+   <Component {...pageProps} /></>
 }
