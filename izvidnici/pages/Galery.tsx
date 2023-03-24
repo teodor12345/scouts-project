935f51d90c11a29a9106f8b2936dac6f80f8ef9f
Teodor Madzovski <ted.madzovski@gmail.com>
2023-03-24 20:06:42 +0100

    5 commit
    save

---
+++ izvidnici/pages/Galery.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+function Galery (){
+
+    return  <div className=" bg-blue-500">
+    <h1 className=" items-center text-center justify-center top-11 left-11">Извиднички Клуб Наум Наумовски-Борче </h1>
+    </div>
+}
+
+export default Galery
--- izvidnici/pages/_app.tsx
+++ izvidnici/pages/_app.tsx
@@ -3,6 +3,6 @@ import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return<>
- <p className=' font-bold'>This id my app lets see what we can do with it and learn something new I hope really hope because it is not easy!!</p>
+ 
    <Component {...pageProps} /></>
 }
