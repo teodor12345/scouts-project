1047ae9ed1fa647811397b1d32ed62053dcf490b
Teodor Madzovski <ted.madzovski@gmail.com>
2023-03-24 17:14:47 +0100

    commit 4
    adding tailwind css

--- izvidnici/pages/index.tsx
+++ izvidnici/pages/index.tsx
@@ -14,30 +14,10 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
+      <main >
+        <section className='  bg-teal-500 min-h-screen'>
+          <h1> Hello World   </h1>
+        </section>
 
       </main>
     </>
