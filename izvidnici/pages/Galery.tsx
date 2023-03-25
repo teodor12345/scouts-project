486e435536b13efafde83484564b9c004a6fb485
Teodor Madzovski <ted.madzovski@gmail.com>
2023-03-25 17:49:11 +0100

    commit 4
    adding homepage

--- izvidnici/pages/Galery.tsx
+++ izvidnici/pages/Galery.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 function Galery (){
 
-    return  <div className=" bg-blue-500">
+    return  <div className=" h-full bg-gradient-to-t from-blue-500">
     <h1 className=" items-center text-center justify-center top-11 left-11">Извиднички Клуб Наум Наумовски-Борче </h1>
     </div>
 }
