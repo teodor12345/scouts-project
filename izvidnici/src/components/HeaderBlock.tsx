fe7e1dfa3edf2b5499138c4b6d1dd1a2ef174a2c
Teodor Madzovski <ted.madzovski@gmail.com>
2023-04-13 16:32:45 +0200

    commit

--- izvidnici/src/components/HeaderBlock.tsx
+++ izvidnici/src/components/HeaderBlock.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import logo from '../public/logo.png'
 
 export const HeaderBlock = () => {
   return (
-    <div>HeaderBlock</div>
+    <div className=" flex h-screen w-full flex-col">
+         <div>
+      <Image 
+         src={logo} alt="placeholder" className=" w-32 h-32 mr-2 p-3"
+      />
+    </div>
+    </div>
   )
 }
 
